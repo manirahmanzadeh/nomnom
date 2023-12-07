06335ae42c3128a642892b0d24a0d2fcd40dbe70
manirahmanzadeh <72001950+manirahmanzadeh@users.noreply.github.com>
2023-12-07 20:15:21 +0330

    feat: added documentation

--- DOCUMENTATION.dart
+++ DOCUMENTATION.dart
@@ -47,7 +47,7 @@
 /// 1. Clone the repository:
 ///
 ///    ```bash
-///    git clone https://github.com/your-username/nomnom.git
+///    git clone https://github.com/manirahmanzadeh/nomnom.git
 ///    ```
 ///
 /// 2. Navigate to the project directory:
